0a6bbb305107ea45f9f6096c5529beb06c6bda1b
kehindave <kehinday3@gmail.com>
2026-04-24 20:01:08 +0100

    VFX graph rendering

--- Assets/Scripts/GrassManager.cs
+++ Assets/Scripts/GrassManager.cs
@@ -1,8 +1,12 @@
 using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
+using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
+using UnityEngine.VFX;
+using UnityEngine.VFX.Utility;
 using Random = UnityEngine.Random;
 
 public class GrassManager : MonoBehaviour
@@ -11,70 +15,66 @@ public class GrassManager : MonoBehaviour
 
     [SerializeField] private Terrain terrain;
     [SerializeField] private int grassDensity = 50;
-    [SerializeField] private Vector3 grassScale, grassOffset;
-    [SerializeField] private Mesh grassMesh;
-    [SerializeField] private Material grassMaterial;
-    private List<Matrix4x4[]> batches = new List<Matrix4x4[]>();
-    void Start()
+    [SerializeField] private Vector3  grassOffset, grassPositionRandomness;
+    [FormerlySerializedAs("grassSIze")] [SerializeField] private float grassSize;
+    [SerializeField] private VisualEffect grassEffect;
+    private GraphicsBuffer grassBuffer;
+
+
+    private void Start()
     {
-        GenerateGrass(new Vector2Int((int)terrain.terrainData.size.x,(int)terrain.terrainData.size.z));
+        GenerateGrass();
     }
-    
-    public void GenerateGrass(Vector2Int bounds)
+
+    [ContextMenu("Generate Grass")]
+    public void GenerateGrass()
     {
-        batches.Clear();
-       
+        grassBuffer?.Dispose();
+        var bounds = new Vector2Int((int)terrain.terrainData.size.x, (int)terrain.terrainData.size.z);
         float stepAmount = 1 / (float)grassDensity;
         Vector2Int pointsPerSide = new Vector2Int(bounds.x * grassDensity, bounds.y * grassDensity);
-        List<Matrix4x4> currentBatch = new List<Matrix4x4>();
+        List<GrassData> blades = new List<GrassData>();
 
         for (int x = 0; x < pointsPerSide.x; x++)
         {
             for (int y = 0; y < pointsPerSide.y; y++)
             {   
-                Vector3 pos = new Vector3(x * stepAmount, 0, (y * stepAmount));
-            
-                Matrix4x4 mat = Matrix4x4.TRS(pos+(grassOffset * Random.Range(0f,1f)), Quaternion.Euler(Random.Range(0,15), Random.Range(0,360), Random.Range(0,15)), grassScale *(Random.Range(1,1.2f)));
-                
-                currentBatch.Add(mat);
+                Vector3 pos = new Vector3(x * stepAmount, 0, (y * stepAmount)) +grassOffset;
+                float angle = Random.Range(0, Mathf.PI * 2);
 
-                // Graphics.DrawMeshInstanced limit is 1023
-                if (currentBatch.Count == 1023)
+                blades.Add(new GrassData()
                 {
-                    batches.Add(currentBatch.ToArray());
-                    currentBatch.Clear();
-                }
+                    position = pos +(grassPositionRandomness * Random.Range(-1f,1f)),
+                    up = new Vector3(Random.Range(-0.15f,0.15f), 1, Random.Range(-0.15f,0.15f)),
+                    forward = new Vector3(Mathf.Cos(angle), 0, Mathf.Sin(angle)),
+                    size = Random.Range(grassSize,grassSize*1.2f)
+                    
+                });
             }
         }
         
-        if (currentBatch.Count > 0) batches.Add(currentBatch.ToArray());
-
+        grassBuffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, blades.Count, UnsafeUtility.SizeOf<GrassData>());
+        grassBuffer.SetData(blades);
+    grassEffect.SetInt("SpawnSize", blades.Count);
+    grassEffect.SetGraphicsBuffer("GrassDataBuffer", grassBuffer);
+    grassEffect.Play();
     
     }
-    
-    void Update()
+
+    private void OnDestroy()
     {
-        // Render every batch every frame
-        foreach (var batch in batches)
-        {
-            Graphics.DrawMeshInstanced(
-                grassMesh, 
-                0, 
-                grassMaterial, 
-                batch, 
-                batch.Length, 
-                null, 
-                UnityEngine.Rendering.ShadowCastingMode.Off, 
-                true
-            );
-        }
+        grassBuffer.Dispose();
     }
 }
 
 [StructLayout(LayoutKind.Sequential)]
-public struct GrassBlade
+[VFXType(VFXTypeAttribute.Usage.GraphicsBuffer)]
+public struct GrassData
 {
     public Vector3 position;
+    public Vector3 forward;
+    public Vector3 up;
+    public float size;
 }
 
 
